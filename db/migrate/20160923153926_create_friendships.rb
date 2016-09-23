cf538b7ebd918a6c56278641ba0734eaea236e48
Patrick Todd <patrick.a.todd@gmail.com>
2016-09-23 16:49:57 -0400

    db fix

---
+++ db/migrate/20160923153926_create_friendships.rb
@@ -0,0 +1,12 @@
+class CreateFriendships < ActiveRecord::Migration[5.0]
+  def change
+    create_table :friendships, id: false do |t|
+      t.integer :user_id
+      t.integer :friend_id
+
+      t.timestamps
+    end
+
+    add_index(:friendships, [:user_id, :friend_user_id], :unique => true)
+  end
+end
--- db/migrate/20160923160058_add_column_to_invitation.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToInvitation < ActiveRecord::Migration[5.0]
-  def change
-    add_column :invitations, :invite_id, :integer
-  end
-end
